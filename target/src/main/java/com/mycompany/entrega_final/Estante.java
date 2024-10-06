c952231c7505eba7bb9c283b0fd707a70141c26f
JavierNT1 <javiernuneslo11@gmail.com>
2024-10-06 17:16:03 -0300

    Descripción de los cambios

---
+++ target/src/main/java/com/mycompany/entrega_final/Estante.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.entrega_final;
+
+/**
+ *
+ * @author javie
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estante {
+    private int codigo;
+    private String nombre;
+    private List<Libro> libros;
+
+    public Estante(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.libros = new ArrayList<>();
+    }
+
+    // Getter para el atributo codigo
+    public int getCodigo() {
+        return codigo;
+    }
+  
+    // Getter para el atributo nombre
+    public String getNombre() {
+        return nombre;
+    }
+  
+    public void agregarLibro(Libro libro) {
+        libros.add(libro);
+    }
+
+    public List<Libro> getLibros() {
+        return libros;
+    }
+
+    @Override
+    public String toString() {
+        return "Estante{" + "codigo=" + codigo + ", nombre=" + nombre + ", libros=" + libros + '}';
+    }
+}
